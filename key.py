1cf5d82a59fd23f4f8170db65fcd22cd076ce366
Your Name <you@example.com>
2013-07-26 22:35:36 +0200

    mim

--- key.py
+++ key.py
@@ -4,11 +4,17 @@ from serial import Serial
 
 br = 38400
 
-try:
-    o = Serial("/dev/ttyACM0", br)
-except Exception as e:
-    print e
-    o = Serial("/dev/ttyACM1", br)
+o=0
+
+for item in ["/dev/ttyACM0", "/dev/ttyACM1", "/dev/ttyUSB0", "/dev/ttyUSB1"]:
+    try:
+        o = Serial(item, br)
+        break
+    except Exception as e:
+        print e
+
+if o == 0:
+	raise (BaseException("dude, wheres your arduino?"))
 
 from curses import *
 
@@ -18,14 +24,16 @@ def digitalwrite(p,v):
     o.write(chr((p<<1)|v))
     i.addstr(0,p,str(v))
 
-colemak_ups  ="qwfpgjluy;[]"
-colemak_downs="arstdhneio'\\"
+colemak_ups   ="qwfpgjluy;[]"
+colemak_downs ="arstdhneio'\\"
+colemak_pulses="zxcvbkm,./"
              
 qwerty_ups  ="qwertyuiop[]"
 qwerty_downs="asdfghjkl;'\\"
 
 ups = colemak_ups
 downs = colemak_downs
+pulses = colemak_pulses
 
 try:
     i.keypad(1)
@@ -37,5 +45,8 @@ try:
 	    digitalwrite(  ups.index(k)+2, 1)
 	if k in downs:
 	    digitalwrite(downs.index(k)+2, 0)
+	if k in pulses:
+	    digitalwrite(pulses.index(k)+2, 1)
+	    digitalwrite(pulses.index(k)+2, 0)
 finally:
     endwin()
